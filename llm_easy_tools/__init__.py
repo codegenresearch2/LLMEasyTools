5b4f79788b28ff9e649ea920af2b46d53dbf804b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-09 04:14:51 +0000

    Reset to 25185ad68f7d2156de6651980f6a355d95cce4f4 and made changes for LLM Round 1

--- llm_easy_tools/__init__.py
+++ llm_easy_tools/__init__.py
@@ -1,2 +1,2 @@
-from llm_easy_tools.schema_generator import get_function_schema, insert_prefix, get_tool_defs, LLMFunction
-from llm_easy_tools.processor import process_response, process_message, process_tool_call, ToolResult
+from .schema_generator import get_function_schema, insert_prefix, get_tool_defs, LLMFunction
+from ..processor import process_response, process_message, process_tool_call, ToolResult
