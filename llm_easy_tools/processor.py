c251d02dfcabc5b536b2f63b83efd308afdc31fb
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 18:01:04 +0000

    Reset to 25185ad68f7d2156de6651980f6a355d95cce4f4 and made changes for LLM Round 1

--- llm_easy_tools/processor.py
+++ llm_easy_tools/processor.py
@@ -21,7 +21,6 @@ class ToolResult:
     error: Optional[Exception] = None
     stack_trace: Optional[str] = None
     soft_errors: list[Exception] = field(default_factory=list)
-    prefix: Optional[BaseModel] = None
     tool: Optional[Union[Callable, BaseModel]] = None
 
     def to_message(self) -> dict[str, str]:
@@ -40,14 +39,13 @@ class ToolResult:
             "content": content,
         }
 
-def process_tool_call(tool_call, functions_or_models, prefix_class=None, fix_json_args=True, case_insensitive=False) -> ToolResult:
+def process_tool_call(tool_call, functions_or_models, fix_json_args=True, case_insensitive=False) -> ToolResult:
     function_call = tool_call.function
     tool_name = function_call.name
     args = function_call.arguments
     soft_errors: list[Exception] = []
     error = None
     stack_trace = None
-    prefix = None
     output = None
     try:
         tool_args = json.loads(args)
@@ -59,19 +57,6 @@ def process_tool_call(tool_call, functions_or_models, prefix_class=None, fix_jso
         else:
             stack_trace = traceback.format_exc()
             return ToolResult(tool_call_id=tool_call.id, name=tool_name, error=e, stack_trace=stack_trace)
-
-    if prefix_class is not None:
-        try:
-            prefix = _extract_prefix_unpacked(tool_args, prefix_class)
-        except ValidationError as e:
-            soft_errors.append(e)
-        prefix_name = prefix_class.__name__
-        if case_insensitive:
-            prefix_name = prefix_name.lower()
-        if not tool_name.startswith(prefix_name):
-            soft_errors.append(NoMatchingTool(f"Trying to decode function call with a name '{tool_name}' not matching prefix '{prefix_name}'"))
-        else:
-            tool_name = tool_name[len(prefix_name + '_and_'):]
 
     tool = None
 
@@ -95,7 +80,6 @@ def process_tool_call(tool_call, functions_or_models, prefix_class=None, fix_jso
         error=error,
         stack_trace=stack_trace,
         soft_errors=soft_errors,
-        prefix=prefix,
         tool=tool,
     )
     return result
@@ -140,14 +124,6 @@ def _is_list_type(annotation):
         return any(_is_list_type(arg) for arg in args)
     return False
 
-def _extract_prefix_unpacked(tool_args, prefix_class):
-    prefix_args = {}
-    for key in list(tool_args.keys()):
-        if key in prefix_class.__annotations__:
-            prefix_args[key] = tool_args.pop(key)
-    prefix = prefix_class(**prefix_args)
-    return(prefix)
-
 def process_response(response: ChatCompletion, functions: list[Union[Callable, LLMFunction]], choice_num=0, **kwargs) -> list[ToolResult]:
     message = response.choices[choice_num].message
     return process_message(message, functions, **kwargs)
@@ -155,7 +131,6 @@ def process_response(response: ChatCompletion, functions: list[Union[Callable, L
 def process_message(
     message: ChatCompletionMessage,
     functions: list[Union[Callable, LLMFunction]],
-    prefix_class=None,
     fix_json_args=True,
     case_insensitive=False,
     executor: Union[ThreadPoolExecutor, ProcessPoolExecutor, None]=None
@@ -169,7 +144,7 @@ def process_message(
         tool_calls = []
     if not tool_calls:
         return []
-    args_list = [(tool_call, functions, prefix_class, fix_json_args, case_insensitive) for tool_call in tool_calls]
+    args_list = [(tool_call, functions, fix_json_args, case_insensitive) for tool_call in tool_calls]
 
     if executor:
         results = list(executor.map(lambda args: process_tool_call(*args), args_list))
@@ -181,7 +156,6 @@ def process_one_tool_call(
         response: ChatCompletion,
         functions: list[Union[Callable, LLMFunction]],
         index: int = 0,
-        prefix_class=None,
         fix_json_args=True,
         case_insensitive=False
     ) -> Optional[ToolResult]:
@@ -189,7 +163,7 @@ def process_one_tool_call(
     if not tool_calls or index >= len(tool_calls):
         return None
 
-    return process_tool_call(tool_calls[index], functions, prefix_class, fix_json_args, case_insensitive)
+    return process_tool_call(tool_calls[index], functions, fix_json_args, case_insensitive)
 
 def _get_tool_calls(response: ChatCompletion) -> list[ChatCompletionMessageToolCall]:
     if hasattr(response.choices[0].message, 'function_call') and (function_call := response.choices[0].message.function_call):
