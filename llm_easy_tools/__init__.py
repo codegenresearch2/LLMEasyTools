ba9dfaa6271a33c57a57544a5295051e5ad24d90
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 13:28:10 +0000

    Reset to 25185ad68f7d2156de6651980f6a355d95cce4f4 and made changes for LLM Round 1

--- llm_easy_tools/__init__.py
+++ llm_easy_tools/__init__.py
@@ -2,11 +2,11 @@ from .schema_generator import get_function_schema, get_tool_defs, LLMFunction
 from .processor import process_response, process_message, process_tool_call, ToolResult
 from .types import ChatCompletion, ChatCompletionMessage, ChatCompletionMessageToolCall
 
-def process_tool_call(tool_call, functions_or_models, fix_json_args=True, case_insensitive=False):
+def process_tool_call(tool_call, functions_or_models, fix_json_args, case_insensitive):
     return process_tool_call(tool_call, functions_or_models, fix_json_args, case_insensitive)
 
 def process_response(response, functions, choice_num=0, **kwargs):
     return process_response(response, functions, choice_num, **kwargs)
 
-def process_message(message, functions, fix_json_args=True, case_insensitive=False, executor=None):
+def process_message(message, functions, fix_json_args, case_insensitive, executor):
     return process_message(message, functions, fix_json_args, case_insensitive, executor)
