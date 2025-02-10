df9a19ceeaeb69538d88e1685f02e4f8f2905d8c
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 10:26:46 +0000

    Reset to 25185ad68f7d2156de6651980f6a355d95cce4f4 and made changes for LLM Round 1

--- llm_easy_tools/schema_generator.py
+++ llm_easy_tools/schema_generator.py
@@ -43,8 +43,6 @@ def tool_def(function_schema: dict) -> dict:
 def get_tool_defs(
         functions: list[Union[Callable, LLMFunction]],
         case_insensitive: bool = False,
-        prefix_class: Union[Type[BaseModel], None] = None,
-        prefix_schema_name: bool = True,
         strict: bool = False
         ) -> list[dict]:
     result = []
@@ -54,8 +52,6 @@ def get_tool_defs(
         else:
             fun_schema = get_function_schema(function, case_insensitive, strict)
 
-        if prefix_class:
-            fun_schema = insert_prefix(prefix_class, fun_schema, prefix_schema_name, case_insensitive)
         result.append(tool_def(fun_schema))
     return result
 
@@ -198,11 +194,7 @@ def is_dict(obj: object) -> TypeGuard[dict[str, object]]:
     # as that check is not worth the performance cost
     return isinstance(obj, dict)
 
-def insert_prefix(prefix_class, schema, prefix_schema_name=True, case_insensitive = False):
-    if not issubclass(prefix_class, BaseModel):
-        raise TypeError(
-            f"The given class reference is not a subclass of pydantic BaseModel"
-        )
+def insert_prefix(schema, prefix_schema_name=True, case_insensitive = False):
     prefix_schema = prefix_class.model_json_schema()
     _recursive_purge_titles(prefix_schema)
     prefix_schema.pop('description', '')
